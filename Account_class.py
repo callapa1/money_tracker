cec6083cfdc432f9753fed0b7662c97b40e1c284
callapa1 <lopezcallapa@gmail.com>
2021-06-24 13:16:46 +0200

    all show functions work with ALL, Expenses, and Income

--- Account_class.py
+++ Account_class.py
@@ -19,43 +19,69 @@ class Account:
             item.describe()
             i += 1
 
-    def show_by_title(self, option):
+    def show_by_title(self, selection):
         # 1 = ALL, 2 = EXPENSES, 3 = INCOME
         items = sorted(self.history, key=lambda item: item.title)
-        if option == 2:
+        if selection == 2:
             items = filter(lambda item: item.amount < 0, items)
-        elif option == 3:
+        elif selection == 3:
             items = filter(lambda item: item.amount > 0, items)
+
         for item in items:
             print("-", end=" ")
             item.describe()
 
-    def show_by_amount(self):
+    def show_by_amount(self, selection):
         items = sorted(self.history, key=lambda item: item.amount)
+        if selection == 2:
+            items = filter(lambda item: item.amount < 0, items)
+        elif selection == 3:
+            items = filter(lambda item: item.amount > 0, items)
+
         for item in items:
             print("-", end=" ")
             item.describe()
 
-    def show_by_month(self):
+    def show_by_month(self, selection):
         items = sorted(self.history, key=lambda item: item.month)
+        if selection == 2:
+            items = filter(lambda item: item.amount < 0, items)
+        elif selection == 3:
+            items = filter(lambda item: item.amount > 0, items)
+
         for item in items:
             print("-", end=" ")
             item.describe()
 
-    def show_by_title_r(self):
+    def show_by_title_r(self, selection):
         items = sorted(self.history, key=lambda item: item.title)[::-1]
+        if selection == 2:
+            items = filter(lambda item: item.amount < 0, items)
+        elif selection == 3:
+            items = filter(lambda item: item.amount > 0, items)
+
         for item in items:
             print("-", end=" ")
             item.describe()
 
-    def show_by_amount_r(self):
+    def show_by_amount_r(self, selection):
         items = sorted(self.history, key=lambda item: item.amount)[::-1]
+        if selection == 2:
+            items = filter(lambda item: item.amount < 0, items)
+        elif selection == 3:
+            items = filter(lambda item: item.amount > 0, items)
+
         for item in items:
             print("-", end=" ")
             item.describe()
 
-    def show_by_month_r(self):
+    def show_by_month_r(self, selection):
         items = sorted(self.history, key=lambda item: item.month)[::-1]
+        if selection == 2:
+            items = filter(lambda item: item.amount < 0, items)
+        elif selection == 3:
+            items = filter(lambda item: item.amount > 0, items)
+
         for item in items:
             print("-", end=" ")
             item.describe()
